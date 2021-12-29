1c76cd61ab20f5aa32c2d4f3ff6659452621b419
Hom <71136930+afkHom@users.noreply.github.com>
2021-12-29 16:53:38 -0500

    Add project files.

---
+++ MalPractice/MalPractice.cpp
@@ -0,0 +1,11 @@
+// MalPractice.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//the purpose and use of this is unclear right now, i just want to do something malicious with it and of course all of the code will be stolen or if it is written by me, shittily at that.
+
+#include <iostream>
+
+int main()
+{
+    
+}
+
+
